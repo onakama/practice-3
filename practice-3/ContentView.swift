d6093e8014f23908596e3c71f189a6099733cf43
sumi <ninzin60en.dev@gmail.com>
2021-03-24 20:45:01 +0900

    Initial Commit

---
+++ practice-3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice-3
+//
+//  Created by taisei sumiyama on 2021/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice-3/practice_3App.swift
@@ -0,0 +1,17 @@
+//
+//  practice_3App.swift
+//  practice-3
+//
+//  Created by taisei sumiyama on 2021/03/24.
+//
+
+import SwiftUI
+
+@main
+struct practice_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
